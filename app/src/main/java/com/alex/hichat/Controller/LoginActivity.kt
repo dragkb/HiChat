5178195979303fd84420a6c9a92ce5134116508a
dragkb <alex_kotsar@outlook.com>
2018-11-06 21:59:38 -0800

    Deleted comment

--- app/src/main/java/com/alex/hichat/Controller/LoginActivity.kt
+++ app/src/main/java/com/alex/hichat/Controller/LoginActivity.kt
@@ -34,7 +34,6 @@ class LoginActivity : AppCompatActivity() {
                         if (findSuccess) {
                             enableSpinner(false)
                             finish()
-                            // Idling resource decrement
                         } else {
                             errorToast()
                             enableSpinner(false)
